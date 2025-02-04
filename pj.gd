8c44ed1a25e6e53c94bdc16028c3419d93456d32
sebhvarg <119546442+Sebhvarg@users.noreply.github.com>
2025-02-03 20:57:28 -0500

    Avance

--- pj.gd
+++ pj.gd
@@ -2,10 +2,12 @@ extends CharacterBody2D
 
 @onready var animats = $AnimatedSprite2D
 @export var speed: float = 150.0  # Velocidad del personaje
-		
+var dialogo_npc_contador = 0  # Contador para cambiar diálogos
+var dialogo_fantasmin_contador = 0  
 func _physics_process(delta):
-	var direction = Vector2.ZERO
 
+	var direction = Vector2.ZERO
+	
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 		animats.play("right")
@@ -24,3 +26,19 @@ func _physics_process(delta):
 
 	velocity = direction * speed
 	move_and_slide()
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if dialogo_npc_contador == 0:
+		Dialogic.start("DialogNPC")  # Primer diálogo
+	elif dialogo_npc_contador == 1:
+		Dialogic.start("Dialogosmostrados")  # Segundo diálogo
+	dialogo_npc_contador += 1  # Aumenta el contador
+
+
+func _on_fantasmin_body_entered(body: Node2D) -> void:
+	Dialogic.start("DialogPHT")
+	pass # Replace with function body.
+
+
+func _on_cofre_body_entered(body: Node2D) -> void:
+	Dialogic.start("DialogCHT")
+	pass # Replace with function body.
